461118b928c311cb369ba2fcec44e0e6dc8d77a8
Jan <grimesarisao@gmail.com>
2017-11-12 11:30:34 +0200

    Update pmxarm_tool.py

--- pmxarm_tool/pmxarm_tool.py
+++ pmxarm_tool/pmxarm_tool.py
@@ -109,7 +109,7 @@ class PMXArmToolPpanel(bpy.types.Panel):
                 layout.operator('pmxarm_tool.fix_my_hips', icon = 'ERROR', 
                                 text = 'Select Armature')
         else:
-            layout.label(text = 'Open model first')
+            layout.label(text = 'Select a MMD model')
 
 
 def register():
